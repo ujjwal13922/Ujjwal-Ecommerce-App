246bf556a09be2433e6550417f73e5247893129b
UJJWAL SHARMA <61770093+ujjwal13922@users.noreply.github.com>
2025-01-31 22:08:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const app = express()
 app.use(cors());  
 app.use(express.json())
 app.use(morgan('dev'))
-app.use(express.static(path.join(__dirname,'./client/build')))
+app.use(express.static(path.join(__dirname,'client')))
 
 //routes
 app.use('/api/v1/auth',authroutes)
@@ -37,7 +37,7 @@ app.use('/api/v1/product',productRoutes)
 
 //rest api
 app.use('*',function(req,res){
-    res.sendFile(path.join(__dirname,'./client/build/index.html'))
+    res.sendFile(path.join(__dirname,'client','index.html'))
 });
 
 //port
